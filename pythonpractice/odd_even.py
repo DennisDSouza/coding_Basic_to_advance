7ef6827dde07a9f47a20b298bc877e64ccfe7179
DennisDSouza <dennis.dsouzadds@gmail.com>
2026-06-13 19:24:38 +0530

    coding push

---
+++ pythonpractice/odd_even.py
@@ -0,0 +1,15 @@
+num=[1,2,3,4,]
+odd=0
+even=0
+for nums in num:
+    if nums %2 ==0:
+        even+=1
+    else:
+        odd+=1
+print("Even numbers count",even)
+print("odd numbers count",odd)
+        
+        
+
+    
+    
---
+++ pythonpractice/panlindrom_strings.py
@@ -0,0 +1,9 @@
+character_panlindrom="malayalam"
+panlindrom_storee=""
+for get in character_panlindrom:
+    panlindrom_storee=get+panlindrom_storee
+    
+if character_panlindrom==panlindrom_storee:
+    print("the panlindrom string is right")
+else:
+    print("the panlindrom string is not right")
